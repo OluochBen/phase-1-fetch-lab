070e155cd1bdeb608192eea5b8551acdd50b5e87
Ben Oluoch <splashmaxsoln@gmail.com>
2025-04-26 14:19:34 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(books => renderBooks(books))
+    .catch(error => console.error('Error fetching books:', error));
 }
 
 function renderBooks(books) {
@@ -15,3 +18,28 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+// The total number of pages of all the books
+fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(books => {
+    const totalPages = books.reduce((sum, book) => sum + book.numberOfPages, 0);
+    console.log(`Total Pages: ${totalPages}`);
+  })
+  .catch(error => console.error('Error fetching books:', error));
+
+  //The 1031st character in the series
+  fetch('https://anapioficeandfire.com/api/characters/1031')
+  .then(response => response.json())
+  .then(character => console.log(character))
+  .catch(error => console.error('Error fetching character:', error));
+
+  //The 5th book in the series
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(books => {
+    const fifthBook = books[4]; // Index 4 corresponds to the 5th book
+    console.log(fifthBook);
+  })
+  .catch(error => console.error('Error fetching books:', error));
+
